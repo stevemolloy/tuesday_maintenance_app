691a3a5279691d995670bd3b02635b25e5686102
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-05-15 23:05:42 +0200

    Populatedb does its job

--- populatedb.js
+++ populatedb.js
@@ -21,7 +21,7 @@ var db = mongoose.connection;
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var comments = []
-var reports = []
+var faults = []
 
 function commentCreate(commenter, comment, datetime, cb) {
   commentdetail = {
@@ -43,64 +43,26 @@ function commentCreate(commenter, comment, datetime, cb) {
   });
 }
 
-// function genreCreate(name, cb) {
-//   var genre = new Genre({ name: name });
-//
-//   genre.save(function (err) {
-//     if (err) {
-//       cb(err, null);
-//       return;
-//     }
-//     console.log('New Genre: ' + genre);
-//     genres.push(genre)
-//     cb(null, genre);
-//   }   );
-// }
-//
-// function bookCreate(title, summary, isbn, author, genre, cb) {
-//   bookdetail = {
-//     title: title,
-//     summary: summary,
-//     author: author,
-//     isbn: isbn
-//   }
-//   if (genre != false) bookdetail.genre = genre
-//
-//   var book = new Book(bookdetail);
-//   book.save(function (err) {
-//     if (err) {
-//       cb(err, null)
-//       return
-//     }
-//     console.log('New Book: ' + book);
-//     books.push(book)
-//     cb(null, book)
-//   }  );
-// }
-//
-//
-// function bookInstanceCreate(book, imprint, due_back, status, cb) {
-//   bookinstancedetail = {
-//     book: book,
-//     imprint: imprint
-//   }
-//   if (due_back != false) bookinstancedetail.due_back = due_back
-//   if (status != false) bookinstancedetail.status = status
-//
-//   var bookinstance = new BookInstance(bookinstancedetail);
-//   bookinstance.save(function (err) {
-//     if (err) {
-//       console.log('ERROR CREATING BookInstance: ' + bookinstance);
-//       cb(err, null)
-//       return
-//     }
-//     console.log('New BookInstance: ' + bookinstance);
-//     bookinstances.push(bookinstance)
-//     cb(null, book)
-//   }  );
-// }
-//
-//
+function faultCreate(reporter, comment, status, datetime, cb) {
+  faultdetail = {
+    reporter: reporter,
+    comment: comment,
+    status: status,
+    datetime: datetime
+  }
+
+  var fault = new FaultReport(faultdetail);
+  fault.save(function (err) {
+    if (err) {
+      cb(err, null)
+      return
+    }
+    console.log('New Fault: ' + fault);
+    faults.push(fault)
+    cb(null, fault)
+  }  );
+}
+
 function createComments(cb) {
   async.parallel([
       function(callback) {
@@ -109,87 +71,33 @@ function createComments(cb) {
       function(callback) {
         commentCreate('Lisa Lynch', "A nother lovely comment", Date.now(), callback);
       },
+      function(callback) {
+        commentCreate('Caitlin Molloy-Lynch', "My comments are the bestest", Date.now(), callback);
+      },
+      function(callback) {
+        commentCreate('Rowan Molloy-Lynch', "But mine are the snyggäst!", Date.now(), callback);
+      },
     ],
     // optional callback
     cb);
 }
 
-// function createBooks(cb) {
-//     async.parallel([
-//         function(callback) {
-//           bookCreate('The Name of the Wind (The Kingkiller Chronicle, #1)', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.', '9781473211896', authors[0], [genres[0],], callback);
-//         },
-//         function(callback) {
-//           bookCreate("The Wise Man's Fear (The Kingkiller Chronicle, #2)", 'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.', '9788401352836', authors[0], [genres[0],], callback);
-//         },
-//         function(callback) {
-//           bookCreate("The Slow Regard of Silent Things (Kingkiller Chronicle)", 'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.', '9780756411336', authors[0], [genres[0],], callback);
-//         },
-//         function(callback) {
-//           bookCreate("Apes and Angels", "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...", '9780765379528', authors[1], [genres[1],], callback);
-//         },
-//         function(callback) {
-//           bookCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...", '9780765379504', authors[1], [genres[1],], callback);
-//         },
-//         function(callback) {
-//           bookCreate('Test Book 1', 'Summary of test book 1', 'ISBN111111', authors[4], [genres[0],genres[1]], callback);
-//         },
-//         function(callback) {
-//           bookCreate('Test Book 2', 'Summary of test book 2', 'ISBN222222', authors[4], false, callback)
-//         }
-//         ],
-//         // optional callback
-//         cb);
-// }
-//
-//
-// function createBookInstances(cb) {
-//     async.parallel([
-//         function(callback) {
-//           bookInstanceCreate(books[0], 'London Gollancz, 2014.', false, 'Available', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[1], ' Gollancz, 2011.', false, 'Loaned', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[2], ' Gollancz, 2015.', false, false, callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Available', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Maintenance', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Loaned', callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[0], 'Imprint XXX2', false, false, callback)
-//         },
-//         function(callback) {
-//           bookInstanceCreate(books[1], 'Imprint XXX3', false, false, callback)
-//         }
-//         ],
-//         // Optional callback
-//         cb);
-// }
-//
-//
-//
+function createFaults(cb) {
+    async.parallel([
+        function(callback) {
+          faultCreate(comments[0].commenter, comments[0], "Fixed", Date.now(), callback);
+        },
+        function(callback) {
+          faultCreate(comments[1].commenter, comments[1], "Won't fix", Date.now(), callback);
+        }
+        ],
+        // optional callback
+        cb);
+}
+
 async.series([
     createComments,
-    // createGenreAuthors,
-    // createBooks,
-    // createBookInstances
+    createFaults,
   ],
   // Optional callback
   function(err, results) {
