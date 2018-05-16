aaf27364d06bd2144b9427f5e835b2286756d7ba
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-05-16 22:52:05 +0200

    postform updated, but may no longer be needed

--- public/javascripts/postform.js
+++ public/javascripts/postform.js
@@ -1,3 +1,12 @@
 function submit_report() {
-  console.log("Hello!");
+  var d = new Date();
+  var form = document.getElementById("report_form");
+  var fullname = form.first_name.value + " " + form.last_name.value;
+  var comment = form.comment.value;
+  var status = "Reported";
+  var timestamp = Date.now();
+
+
+
+  console.log(comment);
 }
