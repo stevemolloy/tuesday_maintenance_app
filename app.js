4fed0e11c39dff39f343c754cf18d3d186cc3abd
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-05-20 16:26:38 +0200

    Adding an API route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
