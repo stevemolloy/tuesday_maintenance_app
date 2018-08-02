fff49bfaafb18b53a92c78149a9967e45331af31
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-08-02 13:54:02 +0200

    Crash the app if 1st connection fails

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.connect(mongoDBurl, {
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
-  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+  connectTimeoutMS: 10000, // Give up initial connection after 2 seconds
   })
   .catch(err => {
     console.error('Initial Mongodb connection failed: ' + err.stack);
