05d5493c4ec10d621185d543280e9740071d04e5
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-09-05 15:47:28 +0200

    Reverting routes/index.js since the changes were no longer necessary

--- routes/index.js
+++ routes/index.js
@@ -27,16 +27,10 @@ router.get('/summary/:val', function(req, res, next) {
   res.redirect('/summary/week_number/' + req.params.val);
 });
 
-router.get('/summary/:searchkey/:sortkey', function(req, res, next) {
-    res.redirect('/summary/' + req.params.searchkey + '/' + req.params.sortkey + '/week_number/1');
-})
-
-router.get('/summary/:searchkey/:searchvalue/:sortkey/:sortorder/', function(req, res, next) {
-  const sort_term = {};
-  sort_term[req.params.sortkey] = req.params.sortorder
+router.get('/summary/:key/:value', function(req, res, next) {
   const search_term = {archived: {$ne: true}};
-  if (req.params.searchkey !== 'all' | req.params.searchvalue !== 'all') {
-    search_term[req.params.searchkey] = req.params.searchvalue;
+  if (req.params.key !== 'all' | req.params.value !== 'all') {
+    search_term[req.params.key] = req.params.value;
   }
 
   var functionStack = [];
@@ -62,7 +56,9 @@ router.get('/summary/:searchkey/:searchvalue/:sortkey/:sortorder/', function(req
   functionStack.push((callback) => {
     MaintenanceTask
       .find(search_term)
-      .sort(sort_term)
+      .sort({
+        week_number: -1
+      })
       .exec(function(err, reports) {
         if (err) return console.error(err);
         data = generate_data(reports);
@@ -75,7 +71,7 @@ router.get('/summary/:searchkey/:searchvalue/:sortkey/:sortorder/', function(req
     (err, data) => {
       if (err) console.log(error);
       var page_title;
-      if (req.params.searchkey === 'archived' & req.params.searchvalue === 'true') {
+      if (req.params.key === 'archived' & req.params.value === 'true') {
         page_title = 'MAX IV: Deleted Tasks';
       }
       else {
@@ -83,7 +79,7 @@ router.get('/summary/:searchkey/:searchvalue/:sortkey/:sortorder/', function(req
       }
       res.render('list_all', {
         title: page_title,
-        selected_week: req.params.searchkey==='week_number' ? req.params.searchvalue : 'all',
+        selected_week: req.params.value,
         linac_data: data[1].linac_data,
         linac_ids: data[1].linac_ids,
         other_data: data[1].other_data,
