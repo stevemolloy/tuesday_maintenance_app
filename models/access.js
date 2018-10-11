4b5fbfffa815d8144ccf977bb702d1b7e64ec7fe
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-10-11 14:25:08 +0200

    Removed unnecessary field in the access schema

--- models/access.js
+++ models/access.js
@@ -6,7 +6,6 @@ var AccessSchema = new Schema(
   {
     datetime: {type: Date, required: true},
     where: {type:String, max: 128},
-    task: {type: String, max: 8192},
     starttime: String,
     endtime: String,
     week_number: {type: Number},
