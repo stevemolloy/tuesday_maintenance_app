7328d58a6d420ac6155f56785a36d02e14d5db1f
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2018-10-11 14:27:03 +0200

    In progress, but getting there...

--- routes/access.js
+++ routes/access.js
@@ -1,19 +1,83 @@
 var AccessDetails = require('../models/access').AccessDetails;
 var currentWeekNumber = require('current-week-number');
-
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/:week_number', function(req, res, next) {
-    AccessDetails
-        .findOne({
-            'week_number': req.params.week_number
-        })
-        .exec(function(err, report) {
-            if (err) return console.error(err);
-            res.send('Hello!');
-        });
-});
+function isInteger(value) {
+  return /^\d+$/.test(value);
+}
+
+function getDefaultDoc(week_number) {
+    return [
+        {
+            datetime: Date.now(),
+            where: 'r11',
+            week_number: week_number
+        },
+        {
+            datetime: Date.now(),
+            where: 'r12',
+            week_number: week_number
+        },
+        {
+            datetime: Date.now(),
+            where: 'r31',
+            week_number: week_number
+        },
+        {
+            datetime: Date.now(),
+            where: 'r3235',
+            week_number: week_number
+        },
+        {
+            datetime: Date.now(),
+            where: 'linac',
+            week_number: week_number
+        },
+        {
+            datetime: Date.now(),
+            where: 'spf',
+            week_number: week_number
+        }
+    ];
+}
+
+function weekNumParamIsGood(week_number) {
+    return !isInteger(week_number) || parseInt(week_number)>52
+}
+
+var getData = function(destination) {
+    return function(req, res, next) {
+        if (weekNumParamIsGood(req.params.week_number)) {
+            res.redirect('/bad-param');
+        }
+        else {
+            AccessDetails
+            .find({
+                'week_number': req.params.week_number
+            })
+            .exec(function(err, reports) {
+                if (err) return console.error(err);
+                if (!reports || reports.length===0) {
+                    AccessDetails.insertMany(getDefaultDoc(req.params.week_number))
+                        .then(function(docs){
+                            res.send(docs);
+                        })
+                        .catch(function(err) {
+                            res.redirect('/bad-param');
+                        });
+                }
+                else {
+                    console.log(destination);
+                    res.send(reports);
+                }
+            });
+        }
+    }
+}
+
+router.get('/edit/:week_number', getData('testing'));
+
+router.get('/:week_number', getData('get'));
 
 module.exports = router;
