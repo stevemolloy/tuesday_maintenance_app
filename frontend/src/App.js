2f5f1acfc74585813b7e4a64d0120d42e5d6efc2
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-08-16 16:57:45 +0200

    Factored out the content function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import './App.css';
+import Content from './Content';
 
 function App() {
   return (
@@ -13,17 +12,7 @@ function App() {
           MAX-IV Maintenance Tasks
         </h1>
       </header>
-      <Container>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
+      <Content/>
     </div>
   );
 }
---
+++ frontend/src/Content.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './App.css';
+
+const Content = () => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Content;
+
