3aebad20a98b2d261d19503a8445fe61c8b399c2
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-08-17 21:50:23 +0200

    Dropdown box for choosing week nums

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './App.css';
 
 const Content = () => {
@@ -10,8 +12,13 @@ const Content = () => {
     <div>
       <Container>
         <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
+          <Col>
+            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+          </Col>
         </Row>
         <Row>
           <Col>1 of 3</Col>
